a4cb5efbaf204fbb83dbd86872067dabc778af3c
roybailey <roybaileybiz@gmail.com>
2017-07-14 06:55:59 +0100

    graph diagram story

---
+++ src/components/GraphChart.v2.0.stories.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { storiesOf, action } from '@kadira/storybook'
+
+import GraphChart from './GraphChart.v2.0'
+import graphData from './GraphChart.data.js'
+require('../App.css')
+
+
+storiesOf('GraphChart v2.0', module)
+  .add('GraphChart fixed size 800x800', () => (
+      <GraphChart onClick={action('clicked')}
+        data={graphData}
+        width={800}
+        height={800}
+        />
+  ))
+  .add('GraphChart 100%x700', () => (
+    <div style={{width: '100%', height: 700}}>
+        <GraphChart onClick={action('clicked')}
+            data={graphData}
+            />
+    </div>
+  ))
+  ;
